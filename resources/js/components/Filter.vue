332d069de7a43bb68354409cd7e7fc0580b174fb
Maxime22440 <maxime.rousseau99@gmail.com>
2025-03-27 19:39:13 +0100

    fr

--- resources/js/components/Filter.vue
+++ resources/js/components/Filter.vue
@@ -5,24 +5,22 @@
       <div class="relative">
         <input type="text" class="hidden">
         <input
-          :id="id"
-          class="block w-full form-control form-control-sm form-input form-input-bordered text-sm px-1"
-          :class="{ 'text-white': (value == null) }"
-          type="text"
-          :dusk="`${filter.name}-daterange-filter`"
-          name="daterangefilter"
-          autocomplete="off"
-          :value="value"
-          :placeholder="placeholder"
-          @keydown="handleInput($event)"
-          @paste.prevent
+            :id="id"
+            class="block w-full form-control form-control-sm form-input form-input-bordered text-sm px-1"
+            :class="{ 'text-white': (value == null) }"
+            type="text"
+            :dusk="`${filter.name}-daterange-filter`"
+            name="daterangefilter"
+            autocomplete="off"
+            :value="value"
+            :placeholder="placeholder"
+            @keydown="handleInput($event)"
+            @paste.prevent
         />
-        <div 
-          v-if="value"
-          class="absolute top-0 right-0 mt-1 mr-1">
-          <button class="bg-transparent"
-            @click="clearFilter"
-          >
+        <div
+            v-if="value"
+            class="absolute top-0 right-0 mt-1 mr-1">
+          <button class="bg-transparent" @click="clearFilter">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
@@ -38,7 +36,9 @@ import debounce from 'lodash/debounce'
 import moment from '../../../dist/js/moment.min'
 import 'moment/locale/fr'
 
+// Force la configuration de Moment.js en français et assigne à l'instance globale
 moment.locale('fr')
+window.moment = moment
 
 export default {
   emits: ['change'],
@@ -67,17 +67,14 @@ export default {
 
   created() {
     this.parseDates()
-
     this.debouncedHandleChange = debounce(() => this.handleChange(), 500)
-
     this.setCurrentFilterValue()
   },
 
   mounted() {
     this.id = 'dateRangeCalendar_' + this.generateId()
-    
     Nova.$on('filter-reset', this.setCurrentFilterValue)
-    
+    // Assurez-vous que cette configuration est appliquée après le chargement complet de Nova
     setTimeout(() => {
       this.initDateRange()
     }, 1);
@@ -95,45 +92,58 @@ export default {
 
   methods: {
     setCurrentFilterValue() {
-      if (this.filter.currentValue) {
-        this.value = this.filter.currentValue
-      } else {
-        this.value = null
-      }
+      this.value = this.filter.currentValue || null
     },
     handleChange() {
       this.$store.commit(`${this.resourceName}/updateFilterState`, {
         filterClass: this.filterKey,
         value: (
-          (this.currentStartDate && this.currentEndDate) ? 
-          (this.currentStartDate.format('YYYY-MM-DD') + ' to ' + this.currentEndDate.format('YYYY-MM-DD')) :
-          null
+            (this.currentStartDate && this.currentEndDate) ?
+                (this.currentStartDate.format('YYYY-MM-DD') + ' - ' + this.currentEndDate.format('YYYY-MM-DD')) :
+                null
         ),
       })
-
       this.$emit('change')
-
       this.currentStartDate = null
       this.currentEndDate = null
     },
     handleInput(e) {
-        return e.preventDefault();
+      return e.preventDefault()
     },
-    initDateRange: function() {
+    initDateRange() {
       const ref = this
-      const idSelector = ('#' + this.id)
+      const idSelector = '#' + this.id
       const minDate = ref.filter.minDate
       const maxDate = ref.filter.maxDate
 
-      console.log('moment.locale()');
-      console.log(moment.locale());
+      console.log('moment.locale():', moment.locale()) // Devrait afficher "fr"
+
+      // Forcer la configuration globale du Date Range Picker
+      if ($.fn.daterangepicker) {
+        $.fn.daterangepicker.defaults.locale = {
+          format: 'DD/MM/YYYY',
+          separator: ' - ',
+          applyLabel: 'Appliquer',
+          cancelLabel: 'Annuler',
+          fromLabel: 'Du',
+          toLabel: 'Au',
+          customRangeLabel: 'Personnalisé',
+          weekLabel: 'S',
+          daysOfWeek: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+          monthNames: [
+            'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+            'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
+          ],
+          firstDay: 1
+        }
+      }
 
       $(idSelector).daterangepicker({
-        "startDate": ref.startDate,
-			  "endDate": ref.endDate,
-        "minDate": (minDate ? moment(minDate) : null),
-        "maxDate": (maxDate ? moment(maxDate) : null),
-        "ranges": ref.parseRanges(),
+        startDate: ref.startDate,
+        endDate: ref.endDate,
+        minDate: (minDate ? moment(minDate) : null),
+        maxDate: (maxDate ? moment(maxDate) : null),
+        ranges: ref.parseRanges(),
         locale: {
           format: 'DD/MM/YYYY',
           separator: ' - ',
@@ -154,56 +164,51 @@ export default {
           ref.currentEndDate = end
         }
       })
-      .on('apply.daterangepicker', function(ev, picker) {
-        if (ref.currentStartDate && ref.currentEndDate) {
-          ref.value = ref.currentStartDate.format('DD/MM/YYYY') + ' - ' + ref.currentEndDate.format('DD/MM/YYYY')
-        }
-      })
+          .on('apply.daterangepicker', function(ev, picker) {
+            if (ref.currentStartDate && ref.currentEndDate) {
+              ref.value = ref.currentStartDate.format('DD/MM/YYYY') + ' - ' + ref.currentEndDate.format('DD/MM/YYYY')
+            }
+          })
     },
-    clearFilter: function () {
+    clearFilter() {
       this.value = null
     },
-    generateId: function () {
-      return Math.random().toString(36).substring(2) +
-        (new Date()).getTime().toString(36);
+    generateId() {
+      return Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36)
     },
-    parseDates: function() {
+    parseDates() {
       const dateRange = this.filter.currentValue
       let startDate = moment()
       let endDate = moment()
 
       if (dateRange) {
         const parsedDateRange = dateRange.split(' to ')
-        if (parsedDateRange.length == 2) {
+        if (parsedDateRange.length === 2) {
           try {
             startDate = moment(parsedDateRange[0], "YYYY-MM-DD")
             endDate = moment(parsedDateRange[1], "YYYY-MM-DD")
           } catch(e){}
         }
       }
-      this.startDate = startDate.format('MM/DD/YYYY')
-      this.endDate = endDate.format('MM/DD/YYYY')
-
+      // Utiliser le format français partout
+      this.startDate = startDate.format('DD/MM/YYYY')
+      this.endDate = endDate.format('DD/MM/YYYY')
       this.currentStartDate = startDate
       this.currentEndDate = endDate
     },
-    parseRanges: function() {
-      const ranges = this.filter.options;
-      let parsedRanges = {};
-
-      for(let i=0; i<ranges.length; i++) {
+    parseRanges() {
+      const ranges = this.filter.options
+      let parsedRanges = {}
+      for (let i = 0; i < ranges.length; i++) {
         parsedRanges[ranges[i]['label']] = [moment(ranges[i][0]), moment(ranges[i][1])]
       }
-
-      return parsedRanges;
+      return parsedRanges
     }
   },
 
   computed: {
     filter() {
-      return this.$store.getters[`${this.resourceName}/getFilter`](
-        this.filterKey
-      )
+      return this.$store.getters[`${this.resourceName}/getFilter`](this.filterKey)
     },
   },
 }
